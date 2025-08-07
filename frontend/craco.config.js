9772ac1ba1f3bdebd29cf85ffa6655c9bb9d0a92
cryptojr28 <cryptojr28@gmail.com>
2025-08-07 21:50:41 +0200

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -2,44 +2,23 @@ const webpack = require('webpack');
 
 module.exports = {
   webpack: {
-    configure: (webpackConfig) => {
-      // Completely disable React Fast Refresh in production
-      if (process.env.NODE_ENV === 'production') {
-        // Remove React Refresh plugin
-        webpackConfig.plugins = webpackConfig.plugins.filter(
-          plugin => 
-            plugin.constructor.name !== 'ReactRefreshWebpackPlugin' &&
-            !plugin.constructor.name.includes('ReactRefresh')
-        );
-
-        // Remove React Refresh loader
-        webpackConfig.module.rules.forEach(rule => {
-          if (rule.oneOf) {
-            rule.oneOf.forEach(oneOfRule => {
-              if (oneOfRule.use && Array.isArray(oneOfRule.use)) {
-                oneOfRule.use = oneOfRule.use.filter(
-                  useEntry => 
-                    !useEntry.loader || 
-                    !useEntry.loader.includes('react-refresh')
-                );
-              }
-            });
-          }
+    configure: (webpackConfig, { env }) => {
+      // Only disable in production
+      if (env === 'production') {
+        // Remove React Refresh Plugin completely
+        webpackConfig.plugins = webpackConfig.plugins.filter(plugin => {
+          return plugin.constructor.name !== 'ReactRefreshWebpackPlugin';
         });
-
-        // Add define plugin to completely disable fast refresh
-        webpackConfig.plugins.push(
-          new webpack.DefinePlugin({
-            'process.env.FAST_REFRESH': 'false'
-          })
-        );
       }
       return webpackConfig;
     }
   },
-  devServer: {
-    hot: false,
-    liveReload: false
+  devServer: (devServerConfig, { env }) => {
+    // Disable hot reload in production
+    if (env === 'production') {
+      devServerConfig.hot = false;
+    }
+    return devServerConfig;
   },
   style: {
     postcss: {
