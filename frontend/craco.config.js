61c0a58858e09f659d0a20e57acef2e8931fbed7
cryptojr28 <cryptojr28@gmail.com>
2025-08-07 21:40:05 +0200

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,18 +1,45 @@
+const webpack = require('webpack');
+
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      // Disable React Fast Refresh in production
+      // Completely disable React Fast Refresh in production
       if (process.env.NODE_ENV === 'production') {
+        // Remove React Refresh plugin
         webpackConfig.plugins = webpackConfig.plugins.filter(
-          plugin => plugin.constructor.name !== 'ReactRefreshWebpackPlugin'
+          plugin => 
+            plugin.constructor.name !== 'ReactRefreshWebpackPlugin' &&
+            !plugin.constructor.name.includes('ReactRefresh')
+        );
+
+        // Remove React Refresh loader
+        webpackConfig.module.rules.forEach(rule => {
+          if (rule.oneOf) {
+            rule.oneOf.forEach(oneOfRule => {
+              if (oneOfRule.use && Array.isArray(oneOfRule.use)) {
+                oneOfRule.use = oneOfRule.use.filter(
+                  useEntry => 
+                    !useEntry.loader || 
+                    !useEntry.loader.includes('react-refresh')
+                );
+              }
+            });
+          }
+        });
+
+        // Add define plugin to completely disable fast refresh
+        webpackConfig.plugins.push(
+          new webpack.DefinePlugin({
+            'process.env.FAST_REFRESH': 'false'
+          })
         );
       }
       return webpackConfig;
     }
   },
   devServer: {
-    hot: process.env.NODE_ENV !== 'production',
-    liveReload: process.env.NODE_ENV !== 'production'
+    hot: false,
+    liveReload: false
   },
   style: {
     postcss: {
